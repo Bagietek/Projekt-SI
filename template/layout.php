fdb77a12648b877c0c175d6d3fbbdc43a795fb26
Apuroksiczek <adhd.skorupa@gmail.com>
2021-11-21 18:30:20 +0100

    piu

--- template/layout.php
+++ template/layout.php
@@ -13,6 +13,7 @@
         <input type="button" name = "przepisy" value="Przepisy">
         <input type="button" name = "forum" value="Forum">
         <input type="button" name = "zaloguj" value="Zaloguj">
+        <input type="button" name = "oNas" value="O nas">
         
         <div class="wyszukaj">
             <input type="search" name="szukaj" placeholder="wyszukaj">
@@ -22,9 +23,105 @@
     </div>
 
     <div class = "witajka">
-        
+        <!-- Można coś dorobić np przepis dnia albo cuś -->
     </div>
 
-    <h1>Hello world</h1>    
+    <div class= "container">
+
+        <div class="left">
+            <p>Najlepsze przepisy</p>
+                <ol>
+                    <li>Placki ziemniaczne</li>
+                    <li>Rosołek</li>
+                    <li>Marchewka Marynowana</li>
+                </ol>
+
+        </div>
+
+        <div class="middle"></div>
+
+        <div class="right"></div>
+
+    </div>
+
+    <!-- <div class="opinieUzytkownikow">
+        <div class="wrapper">
+            <div class="slide">
+                <div class="user">
+                    <img src="" alt="">
+                </div>
+                    <div class="uzytkownikInfo">
+                        <h3>Mariusz Pudzianowski</h3>
+                    </div>
+
+            </div>
+        </div>
+    </div> -->
+
+    <div class = "inspiracje">
+        <h1>Inspiracje</h1>
+    <div class="inspiracjeSrodek">
+        <div class="card">
+            <div class = "obrazek">
+                <h1>Krewetki</h1>
+                <!-- <img src="https://www.mojegotowanie.pl/media/cache/default_view/uploads/media/recipe/0001/99/krewetki-na-ostro.jpeg" alt=""> -->
+            </div>
+                <div class="tresc">
+                    <p>Krewetki smażone na maśle z czosnkiem, 
+                        natką i papryczką chili. Podlewane białym winem, podawane z bagietką. 
+                        Najlepszy i najprostszy sposób na krewetki!</p>
+                </div>
+        </div>
+
+        <div class="card">
+            <div class = "obrazek">
+                <h1>Krewetki</h1>
+                <!-- <img src="https://www.mojegotowanie.pl/media/cache/default_view/uploads/media/recipe/0001/99/krewetki-na-ostro.jpeg" alt=""> -->
+            </div>
+                <div class="tresc">
+                    <p>Krewetki smażone na maśle z czosnkiem, 
+                        natką i papryczką chili. Podlewane białym winem, podawane z bagietką. 
+                        Najlepszy i najprostszy sposób na krewetki!</p>
+                </div>
+        </div>
+
+        <div class="card">
+            <div class = "obrazek">
+                <h1>Krewetki</h1>
+                <!-- <img src="https://www.mojegotowanie.pl/media/cache/default_view/uploads/media/recipe/0001/99/krewetki-na-ostro.jpeg" alt=""> -->
+            </div>
+                <div class="tresc">
+                    <p>Krewetki smażone na maśle z czosnkiem, 
+                        natką i papryczką chili. Podlewane białym winem, podawane z bagietką. 
+                        Najlepszy i najprostszy sposób na krewetki!</p>
+                </div>
+        </div>
+
+        <div class="card">
+            <div class = "obrazek">
+                <h1>Krewetki</h1>
+                <!-- <img src="https://www.mojegotowanie.pl/media/cache/default_view/uploads/media/recipe/0001/99/krewetki-na-ostro.jpeg" alt=""> -->
+            </div>
+                <div class="tresc">
+                    <p>Krewetki smażone na maśle z czosnkiem, 
+                        natką i papryczką chili. Podlewane białym winem, podawane z bagietką. 
+                        Najlepszy i najprostszy sposób na krewetki!</p>
+                </div>
+        </div>
+
+        <div class="card">
+            <div class = "obrazek">
+                <h1>Krewetki</h1>
+                <!-- <img src="https://www.mojegotowanie.pl/media/cache/default_view/uploads/media/recipe/0001/99/krewetki-na-ostro.jpeg" alt=""> -->
+            </div>
+                <div class="tresc">
+                    <p>Krewetki smażone na maśle z czosnkiem, 
+                        natką i papryczką chili. Podlewane białym winem, podawane z bagietką. 
+                        Najlepszy i najprostszy sposób na krewetki!</p>
+                </div>
+        </div>
+    </div>    
+    </div>
+
 </body>
 </html>
