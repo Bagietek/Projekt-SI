d8a760ed2f88aa1150011ef9eaac3bf3a78e8588
wojcikbartosz <w.bartosz@outlook.com>
2021-11-25 01:06:17 +0100

    Dodano opcje: Logowania, wylogowania - więcej zmian w opisie
    do bazy danych dodano użytkownika
    admin o haśle pass. można się zalogować i wylogować. funkcje dawnego layoutu są teraz w pliku home. Do folderu template dodano navbar działający na każdej podstronie

--- views/View.php
+++ views/View.php
@@ -5,7 +5,9 @@ declare(strict_types = 1);
 class View{
 
     public function render($page){
-        require_once('template/layout.php');
+        //require_once('template/layout.php');
+        include('template/navbar.php');
+        include('actions/'.$page.'.php');
     }
 
 }
--- views/login.php
+++ views/login.php
@@ -10,16 +10,19 @@
 </head>
 <body>
 
-<form action="" method="post">
+<form action="index.php" method="post">
 <div class="loginContainer">
     <h2 class="header">Zaloguj</h2>
     <div class="items">
+
         <input type="text" name="login">
         <p>Podaj nazwę użytkownika</p>
         <input type="password" name="password" placeholder="hasło...">
         <p>Podaj Hasło</p>
-        <input type="submit" value="zaloguj">
+        <?php if(isset($_SESSION['error'])&&$_SESSION['error']==1) echo '<br><p style="color:red;">Zła nazwa użytkownika lub hasło!</p>';?>
+        <input type="submit" name="submitlogin" value="zaloguj">
     </div>
+
     
     <a href=""><p>Nie masz konta?</p></a>
 </div>
