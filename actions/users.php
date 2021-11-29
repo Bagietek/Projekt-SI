3efeef05792dc3c90f7f2530ab2b0adbf74f02cd
wojcikbartosz <w.bartosz@outlook.com>
2021-11-29 17:23:56 +0100

    zabezpieczenie przed hackowaniem users z poziomu url

--- actions/users.php
+++ actions/users.php
@@ -1,5 +1,9 @@
 <?php
-
+	if(!isset($_SESSION['logged'])||$_SESSION['permission']!='admin')
+	{
+		$_SESSION['action'] = 'home';
+		header('location: index.php');
+	}
     try 
 	{
 		$db = new PDO('mysql:host=localhost;dbname=siproj', 'root','', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
