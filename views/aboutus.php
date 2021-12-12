d391bc578c5fd02f32890d07157905c87d4ef8e3
Apuroksiczek <82889581+Apuroksiczek@users.noreply.github.com>
2021-12-12 19:48:00 +0100

    Add files via upload
    dodanie stronki "o nas".

---
+++ views/aboutus.php
@@ -0,0 +1,30 @@
+<div class="aboutus-conatiner">
+    <div class="aboutus-left"></div>
+    <div class="aboutus-right">
+        <p>O nas:</p>
+    </div>
+
+    <div class="aboutus-srodek">
+        
+        <div class="aboutus-obarzek"></div>
+        <div class= "aboutus-tresc">
+
+        <span>
+            Witamy,
+            SuperPrzepisy.pl to forum, który powstał z chęci dzielenia się naszą pasją jaką jest gotowanie.
+        </span>
+
+        <p>Jesteśmy studentami III roku Informatyki na Politechnice Śląskiej wydziału Elektrycznego.<p>
+    <p>Po ciężkim dniu zajęć najlepiej relaksujemy się i odpoczywamy właśnie w kuchni, wśród garnków.. produktów spożywczych i całej masy przypraw tworząc „kulinarne dzieła sztuki”.
+    Mamy nadzieję, że każdy kto tu zajrzy znajdzie coś dla siebie..<p>
+   <p> Dla nas to forum będzie pewnego rodzaju wyzwaniem.. od dziś nie gotujemy już tylko dla naszych bliskich i znajomych, od dziś gotujemy też dla Was i z Wami. Mamy nadzieję, że uda nam się sprostać temu wyzwaniu.
+Zapraszamy do wspólnego gotowania.. pieczenia i konsumowania</p>
+
+            
+
+           
+        
+    </div>
+    </div>
+
+</div>
